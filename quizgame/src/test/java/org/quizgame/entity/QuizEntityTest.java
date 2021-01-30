52179b0c66e30e39d033686220c6cce91ec11445
Magnus Enholm <53129079+magnuen2k@users.noreply.github.com>
2021-01-30 02:30:54 +0100

    Les 02 (#3)
    * lesson 2 exercises
    
    * lesson 2 refactor tests

--- quizgame/src/test/java/org/quizgame/entity/QuizEntityTest.java
+++ quizgame/src/test/java/org/quizgame/entity/QuizEntityTest.java
@@ -47,30 +47,7 @@ class QuizEntityTest {
 
     @Test
     public void testQuiz() {
-        Quiz quiz = new Quiz();
-        quiz.setQuestion("Whats nine plus ten?");
-        quiz.setAnswer1("20");
-        quiz.setAnswer2("18");
-        quiz.setAnswer3("21");
-        quiz.setAnswer4("50");
-        quiz.setCorrectAnswerIndex(1);
-
-        // EntityManagerFactory factory = Persistence.createEntityManagerFactory("DB");
-        // EntityManager em = factory.createEntityManager();
-
-        /*EntityTransaction tx = em.getTransaction();
-        tx.begin();
-
-        try {
-            em.persist(quiz);
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-            fail();
-        } finally {
-            em.close();
-            factory.close();
-        }*/
+        Quiz quiz = createQuiz();
 
         assertTrue(persistInATransaction(quiz));
 
@@ -79,72 +56,31 @@ class QuizEntityTest {
 
     @Test
     public void testQuizWithSubCategory(){
-        Category c = new Category();
-        SubCategory sc = new SubCategory();
-        c.setSubCategory(sc);
-        Quiz quiz = new Quiz();
-        quiz.setSubCategory(sc);
+        Category c = createCategory("TestCategory");
+        SubCategory sc = createSubcategory("TestSub", c);
+        Quiz quiz = createQuiz(sc);
 
         assertTrue(persistInATransaction(c, sc, quiz));
     }
 
+
     @Test
     public void testQuery() {
-        Category c = new Category();
-        c.setName("JEE");
 
-        SubCategory sc1 = new SubCategory();
-        sc1.setName("JPA");
-        SubCategory sc2 = new SubCategory();
-        sc2.setName("EJB");
-        SubCategory sc3 = new SubCategory();
-        sc3.setName("JSF");
+        Category JEE = createCategory("JEE");
 
-        c.setSubCategory(sc1);
-        c.setSubCategory(sc2);
-        c.setSubCategory(sc3);
-        sc1.setCategory(c);
-        sc2.setCategory(c);
-        sc3.setCategory(c);
+        SubCategory JPA = createSubcategory("JPA", JEE);
+        SubCategory EJB = createSubcategory("EJB", JEE);
+        SubCategory JSF = createSubcategory("JSF", JEE);
 
-        Quiz quiz1 = new Quiz();
-        quiz1.setQuestion("Whats nine plus ten?");
-        quiz1.setAnswer1("20");
-        quiz1.setAnswer2("18");
-        quiz1.setAnswer3("21");
-        quiz1.setAnswer4("50");
-        quiz1.setCorrectAnswerIndex(1);
+        assertTrue(persistInATransaction(JPA, JEE, EJB, JSF));
 
-        Quiz quiz2 = new Quiz();
-        quiz2.setQuestion("Whats nine plus ten?");
-        quiz2.setAnswer1("20");
-        quiz2.setAnswer2("18");
-        quiz2.setAnswer3("21");
-        quiz2.setAnswer4("50");
-        quiz2.setCorrectAnswerIndex(1);
+        Quiz quiz1 = createQuiz(JPA);
+        Quiz quiz2 = createQuiz(JPA);
+        Quiz quiz3 = createQuiz(EJB);
+        Quiz quiz4 = createQuiz(JSF);
 
-        Quiz quiz3 = new Quiz();
-        quiz3.setQuestion("Whats nine plus ten?");
-        quiz3.setAnswer1("20");
-        quiz3.setAnswer2("18");
-        quiz3.setAnswer3("21");
-        quiz3.setAnswer4("50");
-        quiz3.setCorrectAnswerIndex(1);
-
-        Quiz quiz4 = new Quiz();
-        quiz4.setQuestion("Whats nine plus ten?");
-        quiz4.setAnswer1("20");
-        quiz4.setAnswer2("18");
-        quiz4.setAnswer3("21");
-        quiz4.setAnswer4("50");
-        quiz4.setCorrectAnswerIndex(1);
-
-        quiz1.setSubCategory(sc1);
-        quiz2.setSubCategory(sc1);
-        quiz3.setSubCategory(sc2);
-        quiz4.setSubCategory(sc3);
-
-        persistInATransaction(c, quiz1, quiz2, quiz3, quiz4, sc1, sc2, sc3);
+        assertTrue(persistInATransaction(quiz1, quiz2, quiz3, quiz4));
 
         TypedQuery<Quiz> query = em.createQuery("select q from Quiz q where q.subCategory.name = 'JPA'", Quiz.class);
         List<Quiz> quizzes = query.getResultList();
@@ -156,5 +92,37 @@ class QuizEntityTest {
 
         assertEquals(4, quizzes2.size());
 
+    }
+
+    public Quiz createQuiz() {
+        return createQuiz(null);
+    }
+
+    public Quiz createQuiz(SubCategory subCategory) {
+        Quiz quiz = new Quiz();
+        quiz.setQuestion("Whats nine plus ten?");
+        quiz.setAnswer1("20");
+        quiz.setAnswer2("18");
+        quiz.setAnswer3("21");
+        quiz.setAnswer4("50");
+        quiz.setCorrectAnswerIndex(1);
+        quiz.setSubCategory(subCategory);
+
+        return quiz;
+    }
+
+    public SubCategory createSubcategory(String name, Category category) {
+        SubCategory subCategory = new SubCategory();
+        subCategory.setName(name);
+        subCategory.setCategory(category);
+
+        return subCategory;
+    }
+
+    public Category createCategory(String name) {
+        Category category = new Category();
+        category.setName(name);
+
+        return category;
     }
 }
