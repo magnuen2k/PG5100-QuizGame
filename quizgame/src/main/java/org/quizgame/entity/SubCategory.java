1d3cea3be2494ebfba5deede0c18a469ceb0ee66
magnuen2k <maen030@student.kristiania.no>
2021-02-07 00:13:41 +0100

    exercise 3

--- quizgame/src/main/java/org/quizgame/entity/SubCategory.java
+++ quizgame/src/main/java/org/quizgame/entity/SubCategory.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class SubCategory {
@@ -13,9 +16,12 @@ public class SubCategory {
     @Id @GeneratedValue
     private Long id;
 
+    @NotBlank
+    @Size(max = 128)
     private String name;
 
     @ManyToOne
+    @NotNull
     private Category category;
 
     public void setCategory(Category category) {
